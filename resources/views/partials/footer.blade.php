72754d9c0492cf071e2548a44e6bfe3b935a5b68
Therakar <matteo.pau200@gmail.com>
2023-01-15 23:35:42 +0100

    inserimeto footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,62 +1,79 @@
 <footer>
-    <div class="container d-flex">
 
+    <section id="footer-top">
+        <div class="container d-flex">
+
+            
+            <div class="col-2">
         
-        <div class="col-2">
-    
-          <h3>DC COMICS</h3>
-          <ul class="list-unstyled">
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-          </ul>
-    
-          <h3>SHOP</h3>
-          <ul class="list-unstyled">
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-          </ul>
-    
-        </div>
-    
-        <div class="col-2">
-          <h3>DC</h3>
-          <ul class="list-unstyled">
-            <li>Terms Of Use</li>
-            <li>Privacy Policy (New)</li>
-            <li>Ad Choices</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div class="col-2">
-          <h3>SITES</h3>
-          <ul class="list-unstyled">
-            <li>DC</li>
-            <li>MAD Magazine</li>
-            <li>DC Kids</li>
-            <li>DC Universe</li>
-            <li>DC Power Visa</li>
-
-          </ul>
-        </div>
-
+                <h3>DC COMICS</h3>
+                <ul class="list-unstyled">
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            
+                <h3>SHOP</h3>
+                <ul class="list-unstyled">
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
         
-    </div>
+            </div>
+        
+            <div class="col-2">
+                <h3>DC</h3>
+                <ul class="list-unstyled">
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="col-2">
+                <h3>SITES</h3>
+                <ul class="list-unstyled">
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
 
-    <div class="container container-btm-footer">
-        <p>All Site Content TM and &#169 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
-        <p><a href="#">Cookies Settings</a></p>
-    </div>
+                </ul>
+            </div>
+            
+        </div>
+
+        <div class="container container-btm-footer">
+            <p>All Site Content TM and &#169 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+            <p><a href="#">Cookies Settings</a></p>
+        </div>
+    </section>
+    <section id="footer-bottom">
+        <div class="container d-flex justify-content-between align-items-center">
+            <button class="sign-up-btn">SIGN-UP NOW!</button>
+            <div>
+                <ul class="d-flex align-items-center list-unstyled">
+                    <li><a href="#">FOLLOW US</a></li>
+                    <li><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt=""></li>
+                    <li><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt=""></li>
+                    <li><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt=""></li>
+                    <li><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt=""></li>
+                    <li><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt=""></li>
+                </ul>
+              </div>
+        </div>
+    </section>
        
 </footer>
