ac1ac184f20ca8606883e343f99cb12e057e09b7
Therakar <matteo.pau200@gmail.com>
2023-01-15 21:12:34 +0100

    inserimento top footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,62 @@
 <footer>
+    <div class="container d-flex">
+
+        
+        <div class="col-2">
     
+          <h3>DC COMICS</h3>
+          <ul class="list-unstyled">
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+          </ul>
+    
+          <h3>SHOP</h3>
+          <ul class="list-unstyled">
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+          </ul>
+    
+        </div>
+    
+        <div class="col-2">
+          <h3>DC</h3>
+          <ul class="list-unstyled">
+            <li>Terms Of Use</li>
+            <li>Privacy Policy (New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Ratings</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <h3>SITES</h3>
+          <ul class="list-unstyled">
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+
+          </ul>
+        </div>
+
+        
+    </div>
+
+    <div class="container container-btm-footer">
+        <p>All Site Content TM and &#169 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+        <p><a href="#">Cookies Settings</a></p>
+    </div>
+       
 </footer>
